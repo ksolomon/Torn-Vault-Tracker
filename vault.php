9864688aae38a13b970dde48ed8fc13758112e3e
Keith Solomon <ksolomon@gmail.com>
2022-09-27 09:50:21 -0500

    Feat(api): Correct Log URL
    Switch to correct API URL for the Log call
    
    Issue/Task ID: 1

--- vault.php
+++ vault.php
@@ -7,7 +7,7 @@ echo '<pre>';
 print_r($objMoney);
 echo '</pre>';
 
-$jsonLog = file_get_contents('https://api.torn.com/user/?selections=money&key=LDtibInYMdgVnM9n');
+$jsonLog = file_get_contents('https://api.torn.com/user/?selections=log&key=LDtibInYMdgVnM9n');
 $objLog = json_decode($jsonLog);
 
 echo '<h1>User API - Log</h1>';
